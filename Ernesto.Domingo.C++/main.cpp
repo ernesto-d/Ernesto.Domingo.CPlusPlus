3714463e84190a1fdef7b2691c8635044c847b15
Ernesto <edf00001@red.ujaen.es>
2020-09-26 13:49:28 +0300

    Initial Commit

---
+++ Ernesto.Domingo.C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ernesto.Domingo.C++
+//
+//  Created by Ernesto Domingo Fuentes on 26/09/2020.
+//  Copyright © 2020 Ernesto Domingo Fuentes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
